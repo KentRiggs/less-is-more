facb0b32545a5e6a0124669f2d173c3c96268ebf
kent riggs <k2r3@proton.me>
2024-04-01 14:37:55 -0700

    first draft api - pretest

--- src/server/app.py
+++ src/server/app.py
@@ -1,21 +1,100 @@
 from models import db
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
-from models import metadata
-from flask_restful import Api
-from flask_cors import CORS
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-# Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# Setup Migrate with the app and db instances
 migrate = Migrate(app, db)
 db.init_app(app)
 
+class Users(Resource):
+    def get(self, user_id):
+        user = Users.query.get(user_id)
+        if user is None:
+            return make_response(jsonify(error="User not found"), 404)
+        return make_response(jsonify(user.to_dict()), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_user = Users(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(jsonify(new_user.to_dict()), 201)
+
+    def put(self, user_id):
+        user = Users.query.get(user_id)
+        if user is None:
+            return make_response(jsonify(error="User not found"), 404)
+        
+        data = request.get_json()
+        user.username = data.get('username', user.username)
+        user.email = data.get('email', user.email)
+        user.password = data.get('password', user.password)
+        db.session.commit()
+        return make_response(jsonify(user.to_dict()), 200)
+
+    def delete(self, user_id):
+        user = Users.query.get(user_id)
+        if user is None:
+            return make_response(jsonify(error="User not found"), 404)
+        
+        db.session.delete(user)
+        db.session.commit()
+        return make_response('', 204)
+
+api.add_resource(Users, '/users/<int:user_id>', '/users')
+
+class Apologies(Resource):
+    def get(self, apology_id):
+        apology = Apologies.query.get(apology_id)
+        if apology is None:
+            return make_response(jsonify(error="Apology not found"), 404)
+        
+        apology_data = {
+            "apology_id": apology.apology_id,
+            "user_id": apology.user_id,
+            "text": apology.text,
+            
+            "intended_for": [intended.to_dict() for intended in apology.intended_for],
+            "categories": [category.to_dict() for category in apology.categories]
+        }
+        return make_response(jsonify(apology_data), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_apology = Apologies(user_id=data['user_id'], text=data['text'])
+        db.session.add(new_apology)
+        db.session.commit()
+        return make_response(jsonify(new_apology.to_dict()), 201)
+
+    def put(self, apology_id):
+        apology = Apologies.query.get(apology_id)
+        if apology is None:
+            return make_response(jsonify(error="Apology not found"), 404)
+        
+        data = request.get_json()
+        apology.text = data.get('text', apology.text)
+        apology.user_id = data.get('user_id', apology.user_id)
+        
+        db.session.commit()
+        return make_response(jsonify(apology.to_dict()), 200)
+
+    def delete(self, apology_id):
+        apology = Apologies.query.get(apology_id)
+        if apology is None:
+            return make_response(jsonify(error="Apology not found"), 404)
+        
+        db.session.delete(apology)
+        db.session.commit()
+        return make_response('', 204)
+
+api.add_resource(Apologies, '/apologies/<int:apology_id>', endpoint='apology')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- src/server/models.py
+++ src/server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from datetime import datetime
+from sqlalchemy_serializer import SerializerMixin
+# from flask_bcrypt import Bcrypt
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,8 +10,11 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    serialize_rules = ('-password', 'apologies', '-apologies.user')
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, nullable=False, unique=True)
     username = db.Column(db.String, nullable=False)
@@ -17,11 +22,28 @@ class User(db.Model):
 
     apologies = db.relationship("Apology", backref="user")
 
-    def __repr__(self):
-        return f'<User id={self.id}, username={self.username}>'
+    # @property
+    # def password_hash(self):
+    #     return self._password_hash
+    
+    # @password_hash.setter
+    # def password_hash(self, password):
+    #     byte_object = password.encode('utf-8')
+    #     bcrypt_hash = bcrypt.generate_password_hash(byte_object)
+    #     hash_object_as_string = bcrypt_hash.decode('utf-8')
+    #     self._password_hash = hash_object_as_string
 
-class Apology(db.Model):
+    # def authenticate(self, password):
+    #     return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
+
+    # def __repr__(self):
+    #     return f'<User id={self.id}, username={self.username}>'
+
+class Apology(db.Model, SerializerMixin):
     __tablename__ = 'apologies'
+
+    serialize_rules = ('-user.password', 'intended_for', 'categories.category_name', '-categories.apology')
+
     apology_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     text = db.Column(db.Text, nullable=False)
@@ -34,8 +56,11 @@ class Apology(db.Model):
     def __repr__(self):
         return f'<Apology apology_id={self.apology_id}>'
 
-class IntendedFor(db.Model):
+class IntendedFor(db.Model, SerializerMixin):
     __tablename__ = 'intended_for'
+
+    serialize_rules = ('-apology',)
+
     intended_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
@@ -46,15 +71,21 @@ class IntendedFor(db.Model):
     def __repr__(self):
         return f'<IntendedFor intended_id={self.intended_id}, name={self.name}>'
 
-class Category(db.Model):
+class Category(db.Model, SerializerMixin):
     __tablename__ = 'categories'
+
+    serialize_rules = ('-apology_categories',)
+
     category_id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return f'<Category category_id={self.category_id}, category_name={self.category_name}>'
 
-class ApologyCategory(db.Model):
+class ApologyCategory(db.Model, SerializerMixin):
     __tablename__ = 'apology_categories'
+
+    serialize_rules = ('-apology', '-category')
+
     apology_id = db.Column(db.Integer, db.ForeignKey('apologies.apology_id'), primary_key=True)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id'), primary_key=True)
